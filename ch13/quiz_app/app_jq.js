28abc8661f63c202959b8b74cd0d8c36583bbcb4
devjjongs <whdtjr5689@gmail.com>
2021-07-06 12:04:31 +0900

    1. Practice source add practice source

---
+++ ch13/quiz_app/app_jq.js
@@ -0,0 +1,13 @@
+$(function () {
+    //문제 객체 생성
+    function Question(text, choice, answer) {
+        this.text = text;
+        this.choice = choice;
+        this.answer = answer;
+    }
+    
+    //퀴즈 정보 객체 생성
+    function Quiz(questinos) {
+        
+    }
+})
